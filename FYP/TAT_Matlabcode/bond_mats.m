903c1b443a3375e4764ff30982740f21d1a41ef1
sluke2210 <sluke2210@gmail.com>
2023-09-09 15:35:09 +0100

    Delete FYP/TAT_Matlabcode directory

--- FYP/TAT_Matlabcode/bond_mats.m
+++
@@ -1,7 +0,0 @@
-
-%% determines A and B matrices for a bond (bond angle is the one further along)
-function [ABOND,BBOND]=bond_mats(length, angle, torsion)
-
-%%
-ABOND=make_rot_mat(angle,torsion);
-BBOND=[length;0;0];
--- FYP/TAT_Matlabcode/make_rot_mat.m
+++
@@ -1,7 +0,0 @@
-%% create the rotation matrix for one bond angle and one torsion angle
-function rotationmatrix = make_rot_mat(angle,torsion)
-
-%%
-angle=angle*pi/180.0;
-torsion=torsion*pi/180.0;
-rotationmatrix=[-cos(angle) -sin(angle) 0; sin(angle)*cos(torsion) -cos(angle)*cos(torsion) -sin(torsion); sin(angle)*sin(torsion) -cos(angle)*sin(torsion) cos(torsion)];
